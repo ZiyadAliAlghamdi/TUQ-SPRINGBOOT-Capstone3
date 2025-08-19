0dd9dab187553cde889b4d8f1fdba213457af77d
Faisal <faisalabdulazizz@outlook.com>
2025-08-19 21:41:35 +0300

    created DTO for Booking.java and changed BookingService

---
+++ src/main/java/org/example/capstone3/DTO/BookingDTO.java
@@ -0,0 +1,35 @@
+package org.example.capstone3.DTO;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+@Setter
+@Getter
+@AllArgsConstructor
+public class BookingDTO
+{
+    @NotNull(message = "Booking campaign_id is required")
+    private Integer campaign_id;
+
+    @NotNull(message = "Booking billboard_id is required")
+    private Integer billboard_id;
+    @NotNull(message = "start date cannot be null")
+    @FutureOrPresent(message = "start date must be in the present or future")
+    private LocalDate startDate;
+
+    @NotNull(message = "end date cannot be null")
+    @FutureOrPresent(message = "end date must be in the present or future")
+    private LocalDate endDate;
+
+    @NotNull(message = "price total cannot be null")
+    private Double priceTotal;
+
+    @NotNull(message = "status cannot be null")
+    private String status;
+}
--- src/main/java/org/example/capstone3/Service/BookingService.java
+++ src/main/java/org/example/capstone3/Service/BookingService.java
@@ -2,10 +2,17 @@ package org.example.capstone3.Service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.capstone3.Api.ApiException;
+import org.example.capstone3.DTO.BookingDTO;
+import org.example.capstone3.Model.Billboard;
 import org.example.capstone3.Model.Booking;
+import org.example.capstone3.Model.Campaign;
+import org.example.capstone3.Repository.BillboardRepository;
 import org.example.capstone3.Repository.BookingRepository;
+import org.example.capstone3.Repository.CampaignRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Service
@@ -13,26 +20,39 @@ import java.util.List;
 public class BookingService {
 
     private final BookingRepository bookingRepository;
-
+    private final BillboardRepository billboardRepository;
+    private final CampaignRepository campaignRepository;
     public List<Booking> getAllBooking(){
         return bookingRepository.findAll();
     }
 
-    public void addBooking(Booking booking){
+    public void addBooking(BookingDTO bookingDTO){
+        Billboard billboard = billboardRepository.findBillboardById(bookingDTO.getBillboard_id());
+        Campaign campaign  = campaignRepository.findCampaignById(bookingDTO.getCampaign_id());
+        if(billboard == null || campaign ==null)
+            throw new ApiException("Billboard/campaign not found");
+        Booking booking = new Booking();
+        booking.setBillboard(billboard);
+        booking.setCampaign(campaign);
+        booking.setStatus("pending"); // todo: Need to talk about it
+        booking.setStartDate(bookingDTO.getStartDate());
+        booking.setEndDate(bookingDTO.getEndDate());
+        booking.setPriceTotal(calculateTotalPricePerWeek(booking,billboard));
         bookingRepository.save(booking);
     }
 
-    public void updateBooking(Integer id , Booking booking){
-        Booking booking1 = bookingRepository.findBookingById(id);
-        if (booking1 == null){
+    public void updateBooking(Integer id , BookingDTO booking){
+
+        Booking oldBooking = bookingRepository.findBookingById(id);
+
+        if (oldBooking == null){
             throw new ApiException("Booking with id " + id + " not found");
         }
-        booking1.setStartDate(booking.getStartDate());
-        booking1.setEndDate(booking.getEndDate());
-        booking1.setPriceTotal(booking.getPriceTotal());
-        booking1.setStatus(booking.getStatus());
-        booking1.setCreatedAt(booking.getCreatedAt());
-        bookingRepository.save(booking1);
+        oldBooking.setStartDate(booking.getStartDate());
+        oldBooking.setEndDate(booking.getEndDate());
+        oldBooking.setPriceTotal(booking.getPriceTotal());
+        oldBooking.setStatus(booking.getStatus());
+        bookingRepository.save(oldBooking);
     }
 
     public void deleteBooking(Integer id){
@@ -41,5 +61,17 @@ public class BookingService {
             throw new ApiException("Booking with id " + id + " not found");
         }
         bookingRepository.delete(booking);
+    }
+
+    public Double calculateTotalPricePerWeek(Booking booking, Billboard billboard) {
+        if (booking == null || billboard == null) {
+            throw new ApiException("startDate, endDate, and billboard are required");
+        }
+        if (booking.getEndDate().isBefore(booking.getStartDate())) {
+            throw new ApiException("endDate must be on or after startDate");
+        }
+        long weeks = ChronoUnit.WEEKS.between(booking.getStartDate(), booking.getEndDate());
+
+        return weeks * billboard.getBasePricePerWeek();
     }
 }
